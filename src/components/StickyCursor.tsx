c8eaa64807a94c8fbe18873dfe837459e6e04477
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2025-01-03 16:44:54 +0530

    fix cursor without js

--- src/components/StickyCursor.tsx
+++ src/components/StickyCursor.tsx
@@ -2,8 +2,12 @@ import { motion, useMotionValue, useSpring } from "framer-motion";
 import { useEffect } from "react";
 
 export default function StickyCursor() {
-  const mouse = { x: useMotionValue(0), y: useMotionValue(0) };
-  const CURSOR_SIZE = 80;
+  const mouse = {
+    x: useMotionValue(0),
+    y: useMotionValue(0),
+    size: useMotionValue(0),
+  };
+  const CURSOR_SIZE = 50;
   const mouseMoveHandler = (e: MouseEvent) => {
     mouse.x.set(e.clientX - CURSOR_SIZE / 2);
     mouse.y.set(e.clientY - CURSOR_SIZE / 2);
@@ -19,12 +23,18 @@ export default function StickyCursor() {
   };
   useEffect(() => {
     window.addEventListener("mousemove", mouseMoveHandler);
+    mouse.size.set(CURSOR_SIZE);
     return () => window.removeEventListener("mousemove", mouseMoveHandler);
   }, []);
   return (
     <motion.div
-      style={{ left: springMouse.x, top: springMouse.y }}
-      className="pointer-events-none z-[100] w-[80px] opacity-50 bg-white fixed h-[80px] bg-white rounded-full"
+      style={{
+        left: springMouse.x,
+        top: springMouse.y,
+        width: mouse.size,
+        height: mouse.size,
+      }}
+      className="pointer-events-none z-[100] opacity-50 bg-white fixed bg-white rounded-full"
     />
   );
 }
